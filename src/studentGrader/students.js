2c5a813522ffa0c07003eae53b88d279ef29d1ef
Micah Hauge <micah.hauge@daveramsey.com>
2021-10-21 11:04:55 -0500

    add student grader example

---
+++ src/studentGrader/students.js
@@ -0,0 +1,44 @@
+const students = [
+  {
+    name: "Micah",
+    midtermExamGrade: 90,
+    finalExamGrade: 90,
+    quizAverage: 90,
+    attendance: 100,
+    year: "freshman",
+  },
+  {
+    name: "Alex",
+    midtermExamGrade: 60,
+    finalGrade: 80,
+    quizAvg: 80,
+    attendance: 100,
+    year: "sophomore",
+  },
+  {
+    name: "Hannah",
+    midtermExamGrade: 60,
+    finalGrade: 75,
+    quizAvg: 80,
+    attendance: 100,
+    year: "junior",
+  },
+  {
+    name: "Tim",
+    midtermExamGrade: 67,
+    finalGrade: 85,
+    quizAverage: 82,
+    attendance: 70,
+    year: "senior",
+  },
+  {
+    name: "Josh",
+    midtermExamGrade: 67,
+    finalGrade: 85,
+    quizAvg: 82,
+    attendance: 70,
+    year: "freshmen",
+  },
+];
+
+export { students };
