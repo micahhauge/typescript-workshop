9076859ebcc5baccb22fed71054be60da97b51ba
Micah Hauge <micah.hauge@daveramsey.com>
2021-11-04 13:45:27 -0500

    update average challenge

--- src/challenges/average.js
+++ src/challenges/average.js
@@ -1,5 +1,22 @@
+/**
+ * Instructions:
+ *
+ * Start by converting this file to a TypeScript file.
+ * Then use type annotations to improve the getAverage function.
+ *
+ * NOTE: You do not need to change the body of the function.
+ * Simply add type annotations for the 'numbers' parameter.
+ *
+ * See 'basicTypes.ts' in examples if you need some direction.
+ *
+ * Run this file with "yarn average"
+ */
+
 function getAverage(numbers) {
-  return numbers.sum() / numbers.length;
+  const sum = numbers.reduce((total, number) => total + number);
+  return sum / numbers.length;
 }
 
 console.log(getAverage([1, 2, 3]));
+console.log(getAverage(2));
+console.log(getAverage(1, 2, 3, 4));
